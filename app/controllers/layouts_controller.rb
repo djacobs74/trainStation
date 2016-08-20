572938744c8715d316a0ab9e85d4b679fd3f140b
David Jacobs <djacobs74@gmail.com>
2016-08-20 19:36:58 +0000

    added simpleform

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -2,4 +2,8 @@ class LayoutsController < ApplicationController
     def index
         @layouts = Layout.all
     end
+
+    def new
+        @layout = Layout.new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 TrainStation::Application.routes.draw do
   root 'layouts#index'
+
+  resources :layouts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
